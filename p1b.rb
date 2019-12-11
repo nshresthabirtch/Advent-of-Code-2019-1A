a7a3caf5d9d0b51ee4903a7b471a5ffc02c27607
nshresthabirtch <nshresthabirtch@gmail.com>
2019-12-11 08:44:50 +0000

    Took an attempt at day 1B

--- p1b.rb
+++ p1b.rb
@@ -3,8 +3,9 @@ def p2
  input = File.read("p1.txt")
  fuel = 0
  modules = input.split("\n").map(&:to_i)
+ 
  modules.each do |mass|
-  while mass > 0
+  while mass >= 0
     mass = (mass / 3) - 2
     fuel += mass
   end
@@ -14,4 +15,20 @@ end
 return fuel
 
 end
-#I did this in google sheets and I know im expecting 5193796 :D, but I get 5193672
+
+=begin
+
+I did this in google sheets and I know im expecting 5193796 :D, but I keep getting 5193640.
+
+Trying to use https://www.leighhalliday.com/recursion-in-ruby
+
+def iter_sum(array)
+  sum = 0
+  array.each do |elem|
+    sum += elem
+  end
+  sum
+end
+
+iter_sum([1,2,3,4,5]) # 15
+=end
